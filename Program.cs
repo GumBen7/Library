8762e3d97273da4b4f7380719aaacf4a4d2e7380
Егор Сивцев <gumben7@gmail.com>
2023-03-16 15:24:43 +0700

    final

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
 ﻿namespace BookShelf {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+            
+        }
+        public class Book {
+            public string Title {get; set;}
+        }
+        public static void Check() {
+            var book = new Book();
+            book.Title = "Structure and interpretation of computer programs";
+            Console.WriteLine(book.Title);
         }
     }
 }
